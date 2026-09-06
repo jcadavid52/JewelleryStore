fd67ca7dda8ea2344316c7539606df302ab04ff4
Camilo Cadavid <camilorock.t@hotmail.com>
2026-09-06 17:10:21 -0500

    refactor: implementación de migraciones automáticas opcionales

--- src/Modules/Catalog/JewelleryStore.Modules.Catalog.Infrastructure/Injections/OutputPointDependencyInjection.cs
+++ src/Modules/Catalog/JewelleryStore.Modules.Catalog.Infrastructure/Injections/OutputPointDependencyInjection.cs
@@ -1,19 +1,25 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using JewelleryStore.Modules.Catalog.Domain.OuputPorts;
+using JewelleryStore.Modules.Catalog.Infrastructure.OutputPointAdapters.Database.HostedServices;
 using JewelleryStore.Modules.Catalog.Infrastructure.OutputPointAdapters.Database.SqlServer;
 using JewelleryStore.Modules.Catalog.Infrastructure.OutputPointAdapters.Database.SqlServer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace JewelleryStore.Modules.Catalog.Infrastructure.Injections;
 
 public static class OutputPointDependencyInjection
 {
-    public static IServiceCollection AddCatalogPersistence(this IServiceCollection services, string connectionString)
+    public static IServiceCollection AddCatalogPersistence(this IServiceCollection services, string connectionString, bool applyMigrations)
     {
         services.AddDbContext<CatalogDbContext>(options => options.UseSqlServer(connectionString));
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IUnitOfWork, SqlServerUnitOfWork>();
+
+        if (applyMigrations)
+        {
+            services.AddHostedService<AutomaticMigrationsHostedService>();
+        }
 
         return services;
     }
---
+++ src/Modules/Catalog/JewelleryStore.Modules.Catalog.Infrastructure/OutputPointAdapters/Database/HostedServices/AutomaticMigrationsHostedService.cs
@@ -0,0 +1,37 @@
+﻿using JewelleryStore.Modules.Catalog.Infrastructure.OutputPointAdapters.Database.SqlServer;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JewelleryStore.Modules.Catalog.Infrastructure.OutputPointAdapters.Database.HostedServices
+{
+    public class AutomaticMigrationsHostedService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public AutomaticMigrationsHostedService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var provider = _serviceProvider.CreateScope();
+            var database = provider.ServiceProvider.GetRequiredService<CatalogDbContext>();
+            if (database.Database.IsRelational())
+            {
+                try
+                {
+                    await database.Database.MigrateAsync(cancellationToken);
+                }
+                catch (SqlException ex) when (ex.Number == 1801)
+                {
+                    await database.Database.MigrateAsync(cancellationToken);
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
