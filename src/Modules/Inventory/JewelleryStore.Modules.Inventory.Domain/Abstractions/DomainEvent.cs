c1478e7ebd7f96079452a8e7b58b1727923237f9
Camilo Cadavid <camilorock.t@hotmail.com>
2026-09-13 11:21:10 -0500

    refactor: implementación de abstracciones en el dominio

---
+++ src/Modules/Inventory/JewelleryStore.Modules.Inventory.Domain/Abstractions/DomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using JewelleryStore.Modules.Inventory.Domain.Events;
+
+namespace JewelleryStore.Modules.Inventory.Domain.Abstractions
+{
+    public abstract class DomainEvent<TAggregateId> : IDomainEvent
+         where TAggregateId : notnull
+    {
+        public Guid EventId { get; } = Guid.NewGuid();
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+        public TAggregateId AggregateId { get; internal set; } = default!;
+
+        protected DomainEvent(TAggregateId aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
