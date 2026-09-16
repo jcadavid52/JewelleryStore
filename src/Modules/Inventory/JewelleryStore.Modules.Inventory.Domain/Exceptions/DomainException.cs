abd84d28b5ab89298feaceb088e811647b11351f
Camilo Cadavid <camilorock.t@hotmail.com>
2026-09-16 17:03:09 -0500

    refactor: implementación de excepciones base de dominio

---
+++ src/Modules/Inventory/JewelleryStore.Modules.Inventory.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,16 @@
+﻿namespace JewelleryStore.Modules.Inventory.Domain.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        public virtual int StatusCode => 400;
+
+        protected DomainException(string message) : base(message)
+        {
+        }
+
+        protected DomainException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
